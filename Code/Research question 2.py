9e4e916eee6fc12e40f54d842f61abf24593ddd4
gclini <126836235+gclini@users.noreply.github.com>
2023-03-07 16:35:59 -0800

    Update Research question 2.py
    Updated with changes I've made to this file - still not finished

--- Code/Research question 2.py
+++ Code/Research question 2.py
@@ -2,6 +2,7 @@ import pandas as pd
 from utils import process_conservation_data
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from shapely.geometry import Point
 import seaborn as sns
 sns.set()
 
@@ -9,10 +10,23 @@ sns.set()
 Code to answer research question 2
 How have conservation efforts affected the extinction and endangerment of mammals? 
 """
+# Create a colour map showing the conservation ratings around the world for
+# the countries we have this data for
+conservation_data = process_conservation_data("Conservation Data.txt")
+gdf = gpd.read_file("countries.geo.json")
+# Convert the integers of the conservation rating to floats so that map can be colour coded as gradient
+conservation_data["Num Index"] = conservation_data["Num Index"].astype(float)
+# Merge geometry data and conservation data, and then add geometry column to conservation
+# data DataFrame
+merged_data = conservation_data.merge(gdf, left_on="Country", right_on="name", how="inner")
+merged_data = merged_data[["Country", "Num Index", "geometry"]]
+merged_data = gpd.GeoDataFrame(merged_data, geometry="geometry")
+# Plot a map of the conservation rating in all the countries
+fig, ax = plt.subplots(nrows=1, figsize=(15, 7))
+gdf.plot(color="#EEEEEE", ax=ax)
+merged_data.plot(column="Num Index", ax=ax, legend=True, vmin=0)
+plt.title("Map of Conservation Ratings for Countries with Data")
+plt.savefig("Conservation map.png")
 
-
-
-data = process_conservation_data("Conservation Data.txt")
-print(data)
-
-
+# Create a colour coded map showing the threat levels for animals around
+# the world in 2021
