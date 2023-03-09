810e157b2204ff6aee4c4c52553058d8a9f14ae9
Rhea K <rheak2@uw.edu>
2023-03-08 23:26:37 -0800

    updated but not complete

---
+++ main.py
@@ -0,0 +1,13 @@
+from Research_question_1.py import do_question_1
+from Research_Question_2.py import do_question_2
+from Research_Question_3.py import do_question_3
+
+
+def main():
+    do_question_1()
+    do_question_2()
+    do_question_3()
+
+
+if __name__ == '__main__':
+    main()
