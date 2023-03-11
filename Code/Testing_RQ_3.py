787d187d7bb1792b1444a426ba4f7b6bb26f3c4c
Rhea K <rheak2@uw.edu>
2023-03-11 14:54:45 -0800

    added functions that need to be implemented

--- Code/Testing_RQ_3.py
+++ Code/Testing_RQ_3.py
@@ -6,5 +6,13 @@ which determines how reliable a decision tree regression model is for predicting
 the average change in threat level for a given species over 14 years.
 '''
 
-def test_function_name() -> None:
+import Research_Question_3.py
+import utils.py
+
+def test_manipulate_data() -> None:
+    book_1_df = utils.csv_processing('Code\Book1.csv')
+    species_tl_df = utils.species_threat_level_data_processing(book_1_df)
+
+
+def test_train_and_test_model() -> None:
     pass
