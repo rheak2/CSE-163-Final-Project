dd4d65e02c9f16f7b53d55c78787753413e4a27a
Rhea K <rheak2@uw.edu>
2023-03-11 12:49:44 -0800

    added testing files

--- Code/Testing RQ 1.py
+++ Code/Testing RQ 1.py
@@ -1,12 +1,9 @@
 '''
 Giulia Clini, Elizabeth Karas, and Rhea Kulkarni
 CSE 163 Final Project
-This file tests the functions implemented for the first research question,
-which determined which species were most vulnerable to extinction.
+This file tests all of the functions implemented for the first research question,
+which determines which species in the dataset are most vulnerable to extinction.
 '''
-
-import research_question_1
-
 
 def test_function_name() -> None:
     pass
