ffce7e00067f10ac24c4310f495aa14aa8c6dac1
Elizabeth Karas <elikaras@uw.edu>
2023-03-12 12:33:51 -0700

    currently process big data works, process loc doesnt work

--- process_table _7.py
+++ process_table _7.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 import numpy as np
 from operator import itemgetter
-#hello
+
 
 CLASSES_1 = ['INVERTEBRATES', 'REPTILES', 'BIRDS', 'PLANTS',
              'FUNGI', 'FISHES', 'ORCHIDS', 'LEGUMES', 'AMPHIBIANS',
@@ -34,61 +34,98 @@ CLASSES_2 = ['MAMMALS (Mammalia)', 'BIRDS (Aves)', 'REPTILES (Reptilia)',
              'MILLIPEDES (Diplopoda)', 'FUNGI (Mushrooms and Lichens)']
 
 
+def first_loc(place):
+    """
+    modifies location to only contain the first word
+    """
+    if place != np.nan:
+        place = place.split(',')
+        return(place[0])
+
+
+def update_threat(df, year_1, year_2):
+    cy = 'List (' + str(year_2) + ')'
+    py = 'List (' + str(year_1) + ')'
+    if year_2 != 2007:
+        df[cy] = df[cy].where(df[cy] != 'nan', df[py])
+        return(df[cy])
+    else:
+        nt = ['NT'] * len(df[cy])
+        df[cy] = df[cy].where(df[cy] != 'nan', nt)
+        return(df[cy])
+
+
+def fill_in_threat(df):
+    for col in df.columns[3:]:
+        col = str(col)
+        year = int(col[6:10])
+        prev_yr = year - 1
+        df[col] = update_threat(df, prev_yr, year)
+
+
 def process_loc_data():
     print('running process loc data')
     # clean up data to match other data
-    filename = os.path.join('Table 7. Loc Data', 'msw3-all.pdf')
-    loc = pd.DataFrame()
-    df_1 = pd.DataFrame()
+    filename = os.path.join('Table_7_Loc_Data', 'msw3-all.pdf')
+    # loc = pd.DataFrame()
+    # next_pages = pd.DataFrame()
     df_2 = pd.DataFrame()
-    for i in range(1, 3):
-        hold_df = pd.DataFrame()
-        print('running 1' + str(i))
-        curr_table = cm.read_pdf(filename, flavor='stream',
-                                 pages=str(i),
-                                 table_area=['0, 100, 100, 0'])
-        print('done')
-        hold_df = curr_table[0].df
-        df_1 = df_1.append(hold_df, ignore_index=True)
-    print(df_1)
-    # for i in range(107, 109):
+    # curr_table = cm.read_pdf(filename, flavor='stream',
+    #                          pages=str(1),
+    #                          table_areas=['230, 3000, 1450, 200'])
+    # df_1 = curr_table[0].df
+    # print(df_1)
+    # for i in range(2, 10):
     #     hold_df = pd.DataFrame()
-    #     print('running 2' + str(i))
+    #     print('running 1 ' + str(i))
     #     curr_table = cm.read_pdf(filename, flavor='stream',
     #                              pages=str(i),
-    #                              columns=['358, 882, 965, 1389'])
+    #                              table_areas=['230, 3000, 1450, 200'])
     #     hold_df = curr_table[0].df
-    #     df_2 = df_2.append(hold_df, ignore_index=True)
-    # print('done with loops')
-    # # drop the extra info on top, make row 1 the column names
-    df_1.columns = df_1.iloc[0]
+    #     next_pages = next_pages.append(hold_df, ignore_index=True)
+    # print(next_pages)
+    # df_1 = df_1.append(next_pages)
+    for i in range(107, 108):
+        hold_df = pd.DataFrame()
+        print('running 2 ' + str(i))
+        curr_table = cm.read_pdf(filename, flavor='stream',
+                                 pages=str(i),
+                                 table_areas=['810, 2900, 2000, 300'])
+                                #  columns=['900, 940, 1360'])
+        hold_df = curr_table[0].df
+        df_2 = df_2.append(hold_df, ignore_index=True)
+    print('done with loops')
+    # drop the extra info on top, make row 1 the column names
+    # next_pages['Scientific name'] = next_pages[5] + ' ' + next_pages[6]
+    # next_pages = next_pages[[0, 'Scientific name']]
+    # print(df_1)
+    # # df_1.columns = df_1.iloc[0]
     # # drop column names from data
-    df_1 = df_1.drop([0])
-    # # make row 1 the column names
-    # df_2.columns = df_2.iloc[0]
-    # # drop column names from data
-    # df_2 = df_2.drop([0])
-    print(df_1.columns)
-    # print(df_2.columns)
-    print('df 1')
-    print(df_1)
-    # print(df_1['Genus'])
-    # print(df_1['Extinct?'])
-    # df_1 = df_1[['Genus', 'Species', 'ID']]
+    # df_1 = df_1.drop([0])
+    # df_1 = df_1[['ID', 'Genus', 'Species']]
+    # print(df_1)
+    # print(df_1.columns)
+    # print(df_2)
+    # make row 1 the column names
+    df_2.columns = df_2.iloc[0]
+    # drop column names from data
+    df_2 = df_2.drop([0])
+    df_2['Location'] = df_2['TypeLocality'].apply(first_loc)
+    df_2 = df_2[['ID', 'CommonName', 'Location']]
+    # print(df_2['CommonName'])
     # print('df 1')
     # print(df_1)
     # loc = pd.merge(df_1, df_2,
-    #                on=['ID'], how='left')
+    #                on=['ID'], how='outer')
     # loc['Scientific name'] = loc['Genus'] + ' ' + loc['Species']
-    # loc = loc[['Scientific name', 'CommonName', 'TypeLocality']]
-    # print()
-    # print(loc['Scientific name'])
-    # print()
-    # print(loc['TypeLocality'])
+    # loc = loc[['Scientific name', 'CommonName', 'Location']]
+    # print(loc.columns)
     # loc = loc.rename(columns={'CommonName': 'Common name'})
-    # print(loc['Common name'])
+    # next_pages.to_csv('checking_1.2.csv')
+    # df_1.to_csv('checking_part_1.csv')
+    df_2.to_csv('checking part_2.csv')
     # loc.to_csv('mammal_location_2.csv')
-
+    # print(loc)
 
 def process_big_data() -> pd.DataFrame:
     """
@@ -99,10 +136,10 @@ def process_big_data() -> pd.DataFrame:
     print('running process big data')
     merged_df = pd.DataFrame(columns=['Scientific name'])
     # go through each file in the folder
-    for pdf_name in os.listdir('Table 7. Loc Data'):
-        filename = os.path.join('Table 7. Loc Data', pdf_name)
+    for pdf_name in os.listdir('Table_7_Loc_Data'):
+        filename = os.path.join('Table_7_Loc_Data', pdf_name)
         # if the file is the right pdf
-        if (pdf_name[-4:] == '.pdf') and (pdf_name != 'msw3-all.pdf'):
+        if (pdf_name[-4:] == '.pdf') and (pdf_name != 'msw3-all.pdf') and (pdf_name != 'msw3-all-2.pdf'):
             yr = int(pdf_name[0:4])
             print(yr)
             fin_df = pd.DataFrame()
@@ -208,8 +245,8 @@ def process_big_data() -> pd.DataFrame:
                 # 2008 doesn't have this column name
                 fin_df['Reason for change'] = fin_df['Reason for change'
                                                      ].replace('N', np.nan)
-                fin_df['Reason for change'] = fin_df['Reason for change'
-                                                     ].replace('E', np.nan)
+                # fin_df['Reason for change'] = fin_df['Reason for change'
+                #                                      ].replace('E', np.nan)
                 fin_df['Reason for change'] = fin_df['Reason for change'
                                                      ].replace('', np.nan)
             # don't keep anythign that is data deficient
@@ -220,13 +257,53 @@ def process_big_data() -> pd.DataFrame:
             if yr != 2008:
                 fin_df = fin_df.drop(columns=['Reason for change'])
             merged_df = pd.merge(merged_df, fin_df, how='outer')
+    merged_df = merged_df.apply(fill_in_threat())
+    merged_df = merged_df[['Scientific name', 'Common name', 'Class',
+                           'List (2007)', 'List (2008)', 'List (2009)',
+                           'List (2010)', 'List (2011)', 'List (2012)',
+                           'List (2013)', 'List (2014)', 'List (2015)',
+                           'List (2016)', 'List (2017)', 'List (2018)',
+                           'List (2019)', 'List (2020)', 'List (2021)']]
+    merged_df = fill_in_threat(merged_df)
+    loc_data = pd.read_csv('location.csv')
+    merged_df = pd.merge(merged_df, loc_data, left_on='Scientific name',
+                         right_on='Scientific name', how='outer')
+    # to save to csv for testing purposes
+    merged_df.to_csv('final_combined_data')
     return(merged_df)
-    # to save as CSV, uncomment code below, comment out return
-    # if pdf_name != 'msw3-all.pdf':
-    #     print(pdf_name)
-    #     fin_df.to_csv(pdf_name + '.csv')
 
 
-# process_loc_data(DIRECTORY)
-data = process_big_data(DIRECTORY)
-print(data)
+data = process_loc_data()
+# data = process_big_data()
+# data = pd.read_csv('checking part_2.csv')
+# print(data)
+# print(data['8'])
+# data = pd.read_csv('testing_red_list_processing.cvs')
+# print(data.columns)
+
+
+# modifying the data in each year to match the previous year
+# test_set = pd.read_csv('testing_red_list_processing.cvs')
+# test_set = test_set.astype(str)
+# test_set = test_set[['Scientific name', 'Common name', 'Class',
+#                      'List (2007)', 'List (2008)', 'List (2009)',
+#                      'List (2010)', 'List (2011)', 'List (2012)',
+#                      'List (2013)', 'List (2014)', 'List (2015)',
+#                      'List (2016)', 'List (2017)', 'List (2018)',
+#                      'List (2019)', 'List (2020)', 'List (2021)']]
+# fill_in_threat()
+
+
+# print(test_set)
+# print(test_set['List (2017)'])
+# year = 2017
+# # prev_yr = 'List (2016)'
+# prev_yr = 2016
+# print(test_set['List (2017)'])
+# test_set['List (2017)'] = test_set['List (2017)'].apply(update_threat(prev_yr, year))
+# test_set['List (2017)'] = test_set['List (2017)'].where(test_set['List (2017)'] != 'nan', test_set['List (2016)'])
+# print(test_set['List (2017)'])
+
+
+# test_set['List (2017)'] = test_set['List (2017)'].replace('nan', test_set['List (2016)'], inplace=True)
+# print(test_set['List (2016)'])
