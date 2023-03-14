9bede0a186d86e49ad23eafcca937aa45f6de3be
Rhea K <rheak2@uw.edu>
2023-03-13 23:36:18 -0700

    updated with images folder

--- Code/Testing_RQ_3.py
+++ Code/Testing_RQ_3.py
@@ -7,29 +7,17 @@ is for predicting the average change in threat level for a given species
 over 14 years.
 '''
 
-import Research_Question_3
-import utils
+import rq3
 import pandas as pd
+import test_utils
 
 
-def csv_to_df(file_path: str) -> pd.DataFrame:
-    '''
-    Given the filepath of the csv data file as a str, return
-    the DataFrame that is produced using the manipulate_data
-    function.
-    '''
-    book1_df = pd.read_csv(file_path)
-    print(book1_df)
-    mini_df = book1_df[["Common name", "Class", "List (2007)", "List (2008)",
-                        "List (2009)"]]
-    
-    '''
-    mini_df = mini_df.loc[mini_df['Class'].isin(["amphibians", "beetles", "birds"
-                                                 "fishes", "crustaceans", "invertebrates",
-                                                 "mammals", "reptiles", "plants"])]
-    '''
-    test_tl_df = Research_Question_3.manipulate_data(mini_df)
-    return test_tl_df
+def csv_to_df() -> pd.DataFrame:
+    df = pd.read_csv('Code/Book1.csv')
+    print(df)
+    mini_df = test_utils.csv_processing(df)
+    species_tl_data = test_utils.species_threat_level_data_processing(mini_df)
+    return species_tl_data
 
 
 def test_manipulate_data() -> None:
@@ -37,21 +25,10 @@ def test_manipulate_data() -> None:
     This function tests that the manipulate_data
     function (from the output produced in csv_to_df).
     '''
-    print("doing csv_to_df")
-    species_tl_df = csv_to_df('Code/Book1.csv')
-    print("checking manipulate data")
-    expected_num_rows = 73
-    actual_num_rows = len(species_tl_df)
-    print(expected_num_rows, actual_num_rows)
-    # utils.assert_equals(expected_num_rows, actual_num_rows)
+    species_tl_df = csv_to_df()
     expected_num_cols = 8
     actual_num_cols = len(species_tl_df.columns)
-    print(expected_num_cols, actual_num_cols, species_tl_df.columns)
-    # utils.assert_equals(expected_num_cols, actual_num_cols)
-    expected_avg_tl = -0.333
-    actual_avg_tl = species_tl_df.loc[0, 'Average Yearly TL Change Over Time']
-    print(expected_avg_tl, actual_avg_tl)
-    # utils.assert_equals(expected_avg_tl, actual_avg_tl)
+    test_utils.assert_equals(expected_num_cols, actual_num_cols)
 
 
 def test_train_and_test_model() -> None:
@@ -59,14 +36,14 @@ def test_train_and_test_model() -> None:
     This function tests the train_and_test_model function.
     '''
     print("checking train_and_test_model")
-    species_tl_df = csv_to_df('Code/Book1.csv')
-    trained_tested_df = Research_Question_3.train_and_test_model(species_tl_df)
+    species_tl_df = csv_to_df()
+    trained_tested_df = rq3.train_and_test_model(species_tl_df)
     expected_num_pred = 75
     actual_num_train_pred = len(trained_tested_df['Train Predictions'])
     actual_num_test_pred = len(trained_tested_df['Test Predictions'])
     print(expected_num_pred, actual_num_test_pred, actual_num_train_pred)
-    # utils.assert_equals(expected_num_pred, actual_num_train_pred)
-    # utils.assert_equals(expected_num_pred, actual_num_test_pred)
+    test_utils.assert_equals(expected_num_pred, actual_num_train_pred)
+    test_utils.assert_equals(expected_num_pred, actual_num_test_pred)
 
 
 def main():
